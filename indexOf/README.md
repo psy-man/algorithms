ba8c8e44519c13cd04201da8b0d07327c14991d8
Eugene Filipyev <w.0.w@mail.ru>
2018-02-10 21:11:41 +0200

    Fixed tests for indexOf

--- indexOf/README.md
+++ indexOf/README.md
@@ -5,5 +5,6 @@ Finds the index of element in array
 ```
 indexOf([1, 2, 3], 1); // 0
 indexOf([1, 2, 3], 4); // -1
+indexOf([1, 2, 3, 4, 5, 1], 1, 3); // 5
 ```
   
--- indexOf/indexOf.spec.ts
+++ indexOf/indexOf.spec.ts
@@ -4,4 +4,4 @@ import { indexOf } from './indexOf';
 
 test(t => t.is(indexOf([1, 2, 3], 1), 0));
 test(t => t.is(indexOf([1, 2, 3], 4), -1));
-test(t => t.is(indexOf([1, 2, 3, 4, 5, 1], 3), -1));
+test(t => t.is(indexOf([1, 2, 3, 4, 5, 1], 1, 3), 5));
